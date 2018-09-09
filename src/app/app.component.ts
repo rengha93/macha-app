af2256ed630b274ff58d0b385f766f21a146f0b1
renghanath <rengha93@gmail.com>
2018-09-09 22:49:01 +0530

    machaApp UI templates

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'macha-app';
+
+  constructor(){
+    console.log("app constructor called");
+  }
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    console.log("app OnInit Called")
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule , Routes} from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { FooterComponent } from './footer/footer.component';
+import { MenComponent } from './men/men.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+
+
+const appRoutes:Routes =[
+  {path:'header',component:HeaderComponent},
+  {path:'',redirectTo:'/header',pathMatch: 'full'},
+  {path:'men',component:MenComponent},
+  {path:'login',component:LoginComponent},
+  {path:'**',component:PagenotfoundComponent}
+];
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    FooterComponent,
+    MenComponent,
+    PagenotfoundComponent,
+  ],
+  imports: [
+    RouterModule.forRoot(appRoutes),
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
